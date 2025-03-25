be3d2e201ee90fed4f1bd77aaa231eab12f23b03
setPedro <pedross4789@gmail.com>
2025-03-25 19:27:05 +0100

    feat: grid

--- web/src/consts.ts
+++ web/src/consts.ts
@@ -1,6 +1,14 @@
-const sizes = {
+export const sizes = {
   width: 600,
   height: 400,
+  square: 18,
 };
 
-export default sizes;
+export const colors = {
+  darkTile1: 0x121212, // Very dark charcoal gray
+  darkTile2: 0x1c1c1c, // Almost black
+  gridBorderColor: 0x39ff14, // Fluorescent green
+  humanSnakeColor: 0x2d6a4f, // Darker fluorescent green
+  aiSnakeColor: 0xbd33a4, // Neon purple
+  foodColor: 0xff0011, // Neon red
+};
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,19 +1,72 @@
 import Phaser from "phaser";
-import sizes from "./consts";
+import { sizes, colors } from "./consts";
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "GameScene" });
+  }
+
+  preload() {
+    console.log("JS running");
+    // No assets yet
+  }
+
+  createGrid() {
+    // semi-transparent green rectangle for the border
+    const border = this.add
+      .rectangle(
+        sizes.width / 2,
+        sizes.height / 2,
+        sizes.width,
+        sizes.height,
+        colors.gridBorderColor
+      )
+      .setOrigin(0.5)
+      .setAlpha(0.5);
+
+    const gap = 1;
+    const interGap = 1;
+    const numTilesX = Math.floor(
+      (sizes.width - gap * 2) / (sizes.square + interGap)
+    );
+    const tileWidth =
+      (sizes.width - gap * 2 - (numTilesX - 1) * interGap) / numTilesX;
+    const numTilesY = Math.floor(
+      (sizes.height - gap * 2) / (sizes.square + interGap)
+    );
+    const tileHeight =
+      (sizes.height - gap * 2 - (numTilesY - 1) * interGap) / numTilesY;
+
+    for (let file_i = 0; file_i < numTilesX; file_i++) {
+      for (let rank_i = 0; rank_i < numTilesY; rank_i++) {
+        const color =
+          (file_i + rank_i) % 2 === 0 ? colors.darkTile1 : colors.darkTile2;
+        const rectangle = this.add
+          .rectangle(
+            gap + file_i * (tileWidth + interGap),
+            gap + rank_i * (tileHeight + interGap),
+            tileWidth,
+            tileHeight,
+            color
+          )
+          .setOrigin(0, 0);
+      }
+    }
+  }
+
+  create() {
+    this.createGrid();
+  }
+
+  update() {}
+}
 
 const config = {
   type: Phaser.AUTO,
   width: sizes.width,
   height: sizes.height,
-  //   scene: {
-  //     preload: preload,
-  //     create: create,
-  //     update: update,
-  //   },
+  scene: GameScene,
+  scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH },
 };
 
-function log() {
-  console.log(config.width);
-}
-
-log();
+const game = new Phaser.Game(config);
