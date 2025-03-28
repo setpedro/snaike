8c32aa3c43ba99ee0246ce20c15dcde0087aa16a
setPedro <pedross4789@gmail.com>
2025-03-29 00:05:29 +0100

    can control it with WASD

--- web/src/scenes/GameScene.ts
+++ web/src/scenes/GameScene.ts
@@ -4,6 +4,13 @@ import init, { Rectangle } from "../../public/pkg/snake_spark";
 import { colors, sizes } from "../consts";
 
 class GameScene extends Phaser.Scene {
+  private keys!: {
+    w: Phaser.Input.Keyboard.Key;
+    a: Phaser.Input.Keyboard.Key;
+    s: Phaser.Input.Keyboard.Key;
+    d: Phaser.Input.Keyboard.Key;
+  };
+
   private snake!: Rectangle;
   private snakeGraphics!: Phaser.GameObjects.Rectangle;
   private pixelPosition: [number, number] = [210, 210];
@@ -26,11 +33,23 @@ class GameScene extends Phaser.Scene {
         colors.snake.human
       )
       .setOrigin(0.5);
+
+    this.keys = {
+      w: this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+      a: this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+      s: this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+      d: this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+    };
   }
 
   update() {
     const [dirX, dirY] = this.snake.direction();
     const speed = 2;
+
+    if (this.keys.w.isDown && dirY !== 1) this.snake.move_snake("w");
+    if (this.keys.a.isDown && dirX !== 1) this.snake.move_snake("a");
+    if (this.keys.s.isDown && dirY !== -1) this.snake.move_snake("s");
+    if (this.keys.d.isDown && dirX !== -1) this.snake.move_snake("d");
 
     this.pixelPosition[0] += dirX * speed;
     this.pixelPosition[1] += dirY * speed;
