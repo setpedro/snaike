50ad9f5894233c9df59f1b991040be1c631e78b0
setPedro <pedross4789@gmail.com>
2025-03-28 23:10:46 +0100

    committed from wrong directory lol

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct Rectangle {
+    position: (u32, u32),
+    direction: (i32, i32),
+    speed: u32, // in milliseconds
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+impl Rectangle {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self {
+            position: (210, 210),
+            direction: (1, 0),
+            speed: 100,
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[wasm_bindgen]
+    pub fn move_snake(&mut self, input: char) {
+        self.direction = match input.to_ascii_lowercase() {
+            'w' => (0, -1),
+            'a' => (-1, 0),
+            's' => (0, 1),
+            'd' => (1, 0),
+            _ => self.direction
+        };
+    }
+
+    #[wasm_bindgen]
+    pub fn update_position(&mut self, x: u32, y: u32) {
+        self.position = (x, y);
+    }
+
+    #[wasm_bindgen]
+    pub fn position(&self) -> Vec<u32> {
+        vec![self.position.0, self.position.1]
+    }
+    
+
+    #[wasm_bindgen]
+    pub fn direction(&self) -> Vec<i32> {
+        vec![self.direction.0, self.direction.1]
     }
 }
