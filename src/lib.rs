edda38ca9553ab628be0ab1c17cc8a52db57f4d6
setPedro <pedross4789@gmail.com>
2025-03-30 22:35:32 +0200

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -52,33 +52,30 @@ impl Rectangle {
 
     #[wasm_bindgen]
     pub fn update(&mut self, delta_time: f64) {
+        self.process_input();
+        self.update_movement(delta_time);
+    }
+
+    fn process_input(&mut self) {
         let pressed = self.input_state.pressed;
-        let new_dir = if pressed & 0b0001 != 0 {
-            (0, -1)
-        }
-        // Up
-        else if pressed & 0b1000 != 0 {
-            (1, 0)
-        }
-        // Right
-        else if pressed & 0b0100 != 0 {
-            (0, 1)
-        }
-        // Down
-        else if pressed & 0b0010 != 0 {
-            (-1, 0)
-        }
-        // Left
-        else {
-            self.direction
+        let new_dir = match () {
+            _ if pressed & 0b0001 != 0 => (0, -1), // Up
+            _ if pressed & 0b1000 != 0 => (1, 0),  // Right
+            _ if pressed & 0b0100 != 0 => (0, 1),  // Down
+            _ if pressed & 0b0010 != 0 => (-1, 0), // Left
+            _ => self.direction,                   // Fallback
         };
 
-        if new_dir != self.direction
-            && !(new_dir.0 == -self.direction.0 && new_dir.1 == -self.direction.1)
-        {
+        if self.is_valid_direction(new_dir) {
             self.next_direction = Some(new_dir);
         }
+    }
 
+    fn is_valid_direction(&self, dir: (i32, i32)) -> bool {
+        dir != self.direction && !(dir.0 == -self.direction.0 && dir.1 == -self.direction.1)
+    }
+
+    fn update_movement(&mut self, delta_time: f64) {
         let move_amount = self.speed * self.grid_size as f64 * delta_time;
         let dx = self.target_position.0 - self.visual_position.0;
         let dy = self.target_position.1 - self.visual_position.1;
