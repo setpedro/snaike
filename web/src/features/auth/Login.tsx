86b06749008a2b212e047dee09a8a762fbd22c8a
setPedro <pedross4789@gmail.com>
2025-07-12 15:12:24 +0200

    fix: wrong casing on import

--- web/src/features/auth/Login.tsx
+++ web/src/features/auth/Login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { providers } from "./consts";
-import { authWithOAuth } from "./services/authWithOauth";
+import { authWithOAuth } from "./services/authWithOAuth";
 
 export function Login() {
     const navigate = useNavigate();
