4d050590cd12b7fa7bd0a0ebc1a0a0ac064036da
setPedro <pedross4789@gmail.com>
2025-07-13 10:30:20 +0200

    fix: post-auth redirect in local

--- web/src/features/auth/services/authWithOAuth.ts
+++ web/src/features/auth/services/authWithOAuth.ts
@@ -2,9 +2,11 @@ import { supabase } from "@/features/auth/services/supabaseClient";
 import { Provider } from "@supabase/supabase-js";
 
 export async function authWithOAuth(provider: Provider) {
+    const redirectTo = import.meta.env.VITE_SITE_URL;
+
     const { error } = await supabase.auth.signInWithOAuth({
         provider,
-        options: { redirectTo: window.location.hostname },
+        options: { redirectTo },
     });
     if (error) {
         console.error("OAuth error:", error);
