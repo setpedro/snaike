e77e2485e88e13bb46eddbe04951e49c208e8d56
setPedro <pedross4789@gmail.com>
2025-07-14 23:22:31 +0200

    setup header component

---
+++ web/src/features/shared/components/Header.tsx
@@ -0,0 +1,36 @@
+import { useAuthContext } from "@/features/auth/context/AuthProvider";
+import { useNavigate } from "react-router-dom";
+
+export function Header() {
+    const { session } = useAuthContext();
+    const navigate = useNavigate();
+
+    return (
+        <div className="w-full flex justify-end items-center px-6 py-2 fixed top-0 left-0 z-50">
+            <div className="border flex items-center gap-6">
+                <button
+                    onClick={() => navigate("/")}
+                    className="flex items-center gap-2 text-white/80 hover:text-white transition-colors duration-200 font-medium px-4 py-2 rounded-lg hover:bg-white/10"
+                >
+                    <span>Leaderboard</span>
+                </button>
+
+                {session ? (
+                    <button
+                        onClick={() => navigate("/")}
+                        className="flex items-center gap-2 text-white/80 hover:text-white transition-colors duration-200 font-medium px-4 py-2 rounded-lg hover:bg-white/10"
+                    >
+                        <span>Profile</span>
+                    </button>
+                ) : (
+                    <button
+                        onClick={() => navigate("/login")}
+                        className="flex items-center gap-2 text-white/80 hover:text-white transition-colors duration-200 font-medium px-4 py-2 rounded-lg hover:bg-white/10"
+                    >
+                        <span>Log in</span>
+                    </button>
+                )}
+            </div>
+        </div>
+    );
+}
