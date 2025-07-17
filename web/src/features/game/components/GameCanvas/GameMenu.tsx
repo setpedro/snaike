bfd449a50dbd0e95c7f38920d44edacd53852efb
setPedro <pedross4789@gmail.com>
2025-07-17 13:27:20 +0200

    fix: responsive menu

--- web/src/features/game/components/GameCanvas/GameMenu.tsx
+++ web/src/features/game/components/GameCanvas/GameMenu.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/features/shared/utils";
 import { GameViewMode } from "../../types";
 import { Button } from "@/features/shared/components/Button";
 
@@ -7,13 +8,19 @@ type Props = {
 
 export function MainMenu({ onSelectMode }: Props) {
     return (
-        <div className="absolute top-0 left-0 w-full h-full bg-black/70 backdrop-blur-sm text-white flex justify-center items-center z-10 p-4">
-            <div className="flex flex-col items-center w-full max-w-sm sm:max-w-md gap-2 p-2 sm:p-6 md:p-8 bg-black/50 backdrop-blur-sm rounded-3xl border border-white/20 shadow-2xl">
-                <h2 className="text-center text-2xl sm:text-3xl font-bold bg-gradient-to-r from-emerald-400 to-blue-400 bg-clip-text text-transparent mb-2">
+        <div className="absolute top-0 left-0 w-full h-full bg-black/70 backdrop-blur-sm text-white flex justify-center items-center z-10 sm:p-4">
+            <div
+                className={cn(
+                    "flex flex-col items-center justify-center w-full gap-2 p-2 shadow-2xl rounded-none flex-1",
+                    "sm:flex-none sm:h-auto sm:p-6 md:p-8 sm:max-w-md max-w-sm",
+                    "sm:bg-black/50 sm:backdrop-blur-sm sm:rounded-3xl sm:border border-white/20"
+                )}
+            >
+                <h2 className="text-center text-2xl sm:text-3xl font-bold bg-gradient-to-r from-emerald-400 to-blue-400 bg-clip-text text-transparent mb-2 sm:mb-4">
                     🐍 Snaike
                 </h2>
 
-                <div className="flex flex-col w-full gap-2 px-2 sm:px-0 mt-2">
+                <div className="flex flex-col w-full gap-2 px-2 sm:px-0">
                     <Button
                         onClick={() => onSelectMode("solo")}
                         className="group"
