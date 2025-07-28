445b6e4b49cddd8ec53221508e93ee47d9c51d5d
setPedro <pedross4789@gmail.com>
2025-07-28 22:36:17 +0200

    type

--- web/src/features/leaderboard/types.ts
+++ web/src/features/leaderboard/types.ts
@@ -1,1 +1,11 @@
-export type LeaderboardPlayer = {};
+export type LeaderboardPlayer = {
+    id: string;
+    username: string;
+    avatar_url?: string;
+    provider: "google" | "github" | "twitter";
+    total_wins: number;
+    fastest_win_duration: number;
+    highest_score: number;
+    total_games: number;
+    win_rate: number;
+};
